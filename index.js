fcf839043ea70ca60407f6985b40be9c81382ac7
Tomasl77 <lobgeois.tomas@free.fr>
2018-10-26 15:54:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function popuprgpd()
+{
+w=open('','topic','width=350,height=200,resizable=no');
+w.document.write("1. Préambule : <br /><br />Le présent document constitue un contrat entre vous et la société. Dans le cadre de ce contrat, nous serons désignés par les termes, « nous », « nos » ou « notre ». Vous serez désigné comme « Utilisateur », « vous », « vos » ou « votre ». \
+L\'Utilisateur est une seule et unique personne physique. Ce contrat couvre votre utilisation du forum, sans distinction du moyen utilisé pour y accéder (que ce soit par exemple sur nos sites Internet, nos applications pour mobiles et tablettes, ou tout autre moyen actuel ou futur).\
+En vous inscrivant sur le site, vous devez avoir pris connaissance du présent contrat et l\'inscription vaut acceptation de l\'ensemble des dispositions du présent contrat.\
+<br />\
+Nous ne fournissons aucune garantie quant au Service, autres que les garanties légales fournies dans le cadre de la législation applicable.\
+<br />\
+Le coût de l’utilisation de la technique de communication à distance utilisée est le coût normal facturé à l’Utilisateur par son fournisseur d’accès internet. Ce coût est entièrement supporté par l’Utilisateur.\
+<br /><br />\
+2. RGPD : <br /><br /> « Les informations recueillies vous concernant font l’objet d’un traitement destiné à : ... <br />\
+Pour la finalité suivante :…. <br />\
+Les destinataires de ces données sont : ……<br />\
+La durée de conservation des données est de <br />\
+Vous bénéficiez d’un droit d’accès, de rectification, de portabilité, d\’effacement de celles-ci ou une limitation du traitement. <br />\
+Vous pouvez vous opposer au traitement des données vous concernant et disposez du droit de retirer votre consentement à tout moment en vous adressant à :….. <br />\
+Vous avez la possibilité d’introduire une réclamation auprès d’une autorité de contrôle. ».");
+w.document.close();
+} 
